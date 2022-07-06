5ee61c3d7b9a73fbe1bf462b2bdc1c6159f2ea95
Gerald Bauer <gerald.bauer@gmail.com>
2022-07-06 18:41:31 +0200

    add philips

---
+++ philip-phunks/collection.rb
@@ -0,0 +1,12 @@
+##
+# see
+#     https://opensea.io/collection/philip-phunks
+
+
+COLLECTION = TokenCollection.new( 'philip-phunks', 100,
+                     token_base: 'opensea',
+                     format: '24x24',
+                     source: '512x512',
+                     offset: 1
+)
+
