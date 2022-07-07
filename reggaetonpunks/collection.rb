4491c5592e78e1977fffab7da2667759580c65e1
Gerald Bauer <gerald.bauer@gmail.com>
2022-07-07 18:41:07 +0200

    up

--- reggaetonpunks/collection.rb
+++ reggaetonpunks/collection.rb
@@ -3,7 +3,7 @@
 #     https://opensea.io/collection/reggaetonpunks
 
 
-COLLECTION = TokenCollection.new( 'reggaetonpunks', 80,
+COLLECTION = TokenCollection.new( 'reggaetonpunks', 100,
                      token_base: 'opensea',
                      format: '24x24',
                      source: '512x512',
